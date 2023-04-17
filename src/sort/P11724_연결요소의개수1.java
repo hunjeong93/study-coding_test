e7de7c6bac0182a91363f44e5deaca8aff39f6eb
hunjeong <rhkrgnswjd@naver.com>
2023-04-17 15:34:09 +0900

    23.04.17 hunjeong

---
+++ src/sort/P11724_연결요소의개수1.java
@@ -0,0 +1,53 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class P11724_연결요소의개수1 {
+    static ArrayList<Integer>[] A;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        A = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            A[i] = new ArrayList<Integer>(); //인접리스트 초기화
+        }
+        for (int i = 0; i < m; i++) { //인접리스트 데이터 저장
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            A[start].add(end);
+            A[end].add(start);
+        }
+        int count = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (!visited[i]) {
+                count ++;
+                DFS(i);
+            }
+        }
+        System.out.println("연결 요소의 개수 : " + count);
+    }
+
+    static void DFS(int v) {
+        if (visited[v]){
+            return;
+        }
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+}
---
+++ src/sort/P1260_DFS와BFS1.java
@@ -0,0 +1,70 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P1260_DFS와BFS1 {
+    static ArrayList<Integer>[] A;
+    static boolean visited[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int firstNode = Integer.parseInt(st.nextToken());
+
+        A = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            A[i] = new ArrayList<Integer>(); //인접리스트 초기화
+        }
+        for (int i = 0; i < m; i++) { //인접리스트 데이터 저장
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            A[start].add(end);
+            A[end].add(start);
+        }
+        for (int i = 1; i < n + 1; i++) {
+            Collections.sort(A[i]);
+        }
+
+        DFS(firstNode);
+        System.out.println();
+        visited = new boolean[n + 1];
+        BFS(firstNode);
+        System.out.println();
+    }
+
+    static void DFS(int Node) {
+        System.out.print(Node + " ");
+        visited[Node] = true;
+        for (int i : A[Node]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+
+    static void BFS(int Node) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(Node);
+        visited[Node] = true;
+
+        while (!queue.isEmpty()) {
+            int now_Node = queue.poll();
+            System.out.print(now_Node + " ");
+            for (int i : A[now_Node]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
