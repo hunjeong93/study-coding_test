f6189d2287621ee92d7a5986039dbfbaf74f4697
hunjeong <rhkrgnswjd@naver.com>
2023-05-09 20:33:18 +0900

    23.05.09 hunjeong

---
+++ src/graph/P11404_플로이드1.java
@@ -0,0 +1,51 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11404_플로이드1 {
+    static int N, M;
+    static int distance[][];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        distance = new int[N + 1][N + 1];
+        for (int i = 1; i < N + 1; i++) { //인접 행렬 초기화
+            for (int j = 1; j <N + 1; j++) {
+                if (i == j)
+                    distance[i][j] =0;
+                else
+                    distance[i][j] = 1000001; //충분히 큰 수
+            }
+        }
+        for (int k = 1; k < M + 1; k++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (distance[s][e] > v)
+                distance[s][e] = v;
+        }
+        for (int k = 1; k < N + 1; k++){
+            for(int i = 1; i < N + 1; i++){
+                for (int j = 1; j < N + 1; j++){
+                    if (distance[i][j] > distance[i][k] + distance[k][j])
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                }
+            }
+        }
+        for (int i = 1; i < N + 1; i++){
+            for (int j = 1; j < N + 1; j++){
+                if (distance[i][j] == 1000001)
+                    System.out.print("0 ");
+                else
+                    System.out.print(distance[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
